df80ea3df89576cd90ab91ebb3a4b06ad4be219f
Viktor Jakovlev <vicebad@hotmail.com>
2023-03-27 20:23:34 +0200

    exercise 1 added

---
+++ G3/class01/code/ClassExercises/ClassExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿List<string> names = new List<string>();
+
+while(true)
+{
+    Console.WriteLine("Please enter a name, and press x to exit.");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() != "x")
+    {
+        names.Add(input);
+    }
+
+    if (input.ToLower() == "x") 
+    {
+        break;
+    }
+}
+
+string inputText = "";
+Console.WriteLine("Please enter a text:");
+
+string textInput = Console.ReadLine();
+List<string> splittedText = textInput.Split(" ").ToList();
+
+foreach (string name in names) 
+{
+    int count = splittedText.Count((word) => string.Equals(word, name));
+    Console.WriteLine($"{name} : {count}");
+}
+
+
+
+
+
