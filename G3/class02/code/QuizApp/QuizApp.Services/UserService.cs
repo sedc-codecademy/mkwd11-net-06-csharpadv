f549ee8b8d8cede0621bbef9dc5ff06d8076f0ff
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-01 10:34:29 +0200

    quizz app updated

---
+++ G3/class02/code/QuizApp/QuizApp.Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using QuizApp.Domain;
+using QuizApp.Domain.Models;
+
+namespace QuizApp.Services
+{
+    public class UserService
+    {
+        public Student GetStudentByUsername(string username) 
+        {
+            return InMemoryDb.Students.FirstOrDefault(student =>
+                student.Username.ToLower() == username.ToLower());
+        }
+
+        public Teacher GetTeacherByUserName(string username) 
+        {
+            return InMemoryDb.Teachers.FirstOrDefault(teacher =>
+                teacher.Username.ToLower() == username.ToLower());
+        }
+
+        public List<Student> GetStudentsThatDidTheQuiz() 
+        {
+            return InMemoryDb.Students.Where(student =>
+                student.HasFinishedQuiz).ToList();
+        }
+
+        public List<Student> GetStudentThatDidNotFinishTheQuiz() 
+        {
+            return InMemoryDb.Students.Where(student =>
+                !student.HasFinishedQuiz).ToList();
+        }
+
+    }
+}
--- G3/class02/code/QuizApp/QuizApp/Program.cs
+++ G3/class02/code/QuizApp/QuizApp/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using QuizApp.Domain;
+using QuizApp.Domain.Models;
+using QuizApp.Services;
+
+UserService userService = new UserService();
+
+while (true) 
+{
+	try
+	{
+        Console.WriteLine("Enter username:");
+        string username = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(username))
+        {
+            throw new Exception("You must enter valid username.");
+        }
+
+        Teacher teacher = userService.GetTeacherByUserName(username);
+
+        if (teacher != null) { }
+
+        Console.ReadLine();
+    }
+	catch (Exception ex)
+	{
+        Console.WriteLine("An error occured!");
+        Console.WriteLine(ex.Message);
+    }
+
+} 
