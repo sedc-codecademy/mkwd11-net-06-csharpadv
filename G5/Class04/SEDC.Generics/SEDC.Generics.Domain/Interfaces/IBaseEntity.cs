5409984c77d06f7704e7f983fe519878856b9abd
Danilo Borozan <daniloborozan07@gmail.com>
2023-04-03 21:03:01 +0200

    Add code for generic

---
+++ G5/Class04/SEDC.Generics/SEDC.Generics.Domain/Interfaces/IBaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Generics.Domain.Interfaces
+{
+    public interface IBaseEntity
+    {
+        string GetInfo();
+    }
+}
---
+++ G5/Class04/SEDC.Generics/SEDC.Generics.Domain/Models/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.Generics.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Generics.Domain.Models
+{
+    public abstract class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+
+        public abstract string GetInfo();
+    }
+}
