a4ad31359a5ec1f98cff73cfd89f8eeeac3555af
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-05 18:50:19 +0200

    class 3 updated

---
+++ G3/class03/code/AbstractClassesAndInterfaces/AbstractClassesAndInterfaces/Entities/Classes/Developer.cs
@@ -0,0 +1,34 @@
+﻿using AbstractClassesAndInterfaces.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAndInterfaces.Entities.Classes
+{
+    public class Developer : Human, IDeveloper
+    {
+        public List<string> ProgrammingLanguages { get; set; }
+        public int YearsOfExperience { get; set; }
+
+        public Developer(string fullname, 
+                        int age, long phone, 
+                        List<string> languages, 
+                        int experience) : base(fullname, age, phone)
+        {
+            ProgrammingLanguages = languages;
+            YearsOfExperience = experience;
+        }
+
+        public void Code()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetInfo()
+        {
+            return $"{FullName} ({Age}) - {YearsOfExperience} years of experience";
+        }
+    }
+}
