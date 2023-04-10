cbe8b4316b8d31c9a74e22440eb97d7b97e99008
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-10 19:26:32 +0200

    abstract classes example added

---
+++ G3/class05/code/BankAccountApp/BankAccountApp/State/BankAccountState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApp.State
+{
+    public abstract class BankAccountState
+    {
+        public BankAccount BankAccount { get; protected set; }
+
+        public decimal Balance { get; set; }
+
+        public abstract void Deposit(decimal amount);
+        public abstract void Withdraw(decimal amount);
+    }
+}
