24cb9c3bd019707cda63ec8686e561afb723ae2c
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-10 20:37:15 +0200

    updates

---
+++ G3/class05/code/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,30 @@
+﻿using Polymorphism;
+using Polymorphism.Models;
+
+var dog = new Dog()
+{
+    Name = "Majlo",
+    IsGoodBoy = true
+};
+
+dog.Eat();
+
+
+
+var cat = new Cat()
+{
+    Name = "Garfiled",
+    IsLazy = true
+};
+
+cat.Eat();
+
+
+var petService = new PetService();
+
+petService.PetStatus(dog, "Viktor");
+petService.PetStatus("Viktor", dog);
+petService.PetStatus(cat, "Dragan");
+petService.PetStatus(cat);
+
+petService.PetStatus()
