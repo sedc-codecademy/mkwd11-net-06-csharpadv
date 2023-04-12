d5871ce5e01237c25194a61e56d9bf15eb7287ff
Danilo Borozan <daniloborozan07@gmail.com>
2023-04-12 21:08:07 +0200

    Add code for delegate

---
+++ G5/Class07/SEDC.Class07.Delegate/SEDC.Class07.Delegate/Program.cs
@@ -0,0 +1,49 @@
+﻿void SayHello(string name)
+{
+    Console.WriteLine($"Hello {name}");
+}
+
+void SayBye(string name)
+{
+    Console.WriteLine($"Bye {name}");
+}
+
+Console.WriteLine("==== Instantinting a delegate with method");
+SayDelegate bob = new SayDelegate(SayHello);// The SayDelegate points to SayHello
+SayDelegate sayBye = new SayDelegate(SayBye);// The SayDelegate points to SayBye
+SayDelegate anonymousMethod = new SayDelegate(x=>Console.WriteLine($"Zdravo {x}"));// The SayDelegate points to an anonymous method
+
+// Using the delegate ( Points to methods )
+bob("Bob");
+sayBye("Angel");
+anonymousMethod("Mitko");
+
+Console.WriteLine("====== Passing a Method =======");
+
+void DelegateSay(string message, SayDelegate sayDelegate)
+{
+    Console.WriteLine("Delegate say:");
+    sayDelegate(message);
+}
+
+
+DelegateSay("Bob", SayHello);
+DelegateSay("Dimitar", SayBye);
+DelegateSay("Anne", x => Console.WriteLine($"Stuff with {x}"));
+
+Console.WriteLine("====== Making a high order method ======");
+
+void Number(int num1, int num2, NumbersDelegate numbersDelegate)
+{
+    Console.WriteLine( $"Delegate calculate:{numbersDelegate(num1,num2)}");
+}
+
+Number(1, 2, (x, y) => x + y);
+
+// Declaring delegates
+// This is the signature of any method that we want this delegate to point to
+delegate void SayDelegate(string something);
+delegate int NumbersDelegate(int numOne, int numTwo);
+
+
+
