dfaa4ddce61ef2de7f4fa43410374a5772629e4a
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-12 20:45:58 +0200

    class 06 finished, homework added

---
+++ G3/class06/code/ExtensionMethods/ExtensionMethods/Helpers/ListHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.Helpers
+{
+    public static class ListHelper
+    {
+        public static void GoTrough<T>(this List<T> items)
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void GetInfoFor<T>(this List<T> items)
+        {
+            T firstElement = items[0];
+            Console.WriteLine($"This list has {items.Count} " +
+                $"members and is a type of {firstElement.GetType().Name}");
+        }
+    }
+}
--- G3/class06/code/Generics/Generics/Program.cs
+++ G3/class06/code/Generics/Generics/Program.cs
@@ -1,33 +1,31 @@
 ﻿using Generics.Helpers;
 using Generics.Models;
 
-//List<string> strings = new List<string>() { "str1", "str2", "str3" };
-//List<int> ints = new List<int>() { 5, 22, -18 };
-//List<bool> bools = new List<bool>() { true, false, false, true };
+List<string> strings = new List<string>() { "str1", "str2", "str3" };
+List<int> ints = new List<int>() { 5, 22, -18 };
+List<bool> bools = new List<bool>() { true, false, false, true };
 
-//var listHelper = new NoneGenericListHelper();
+var listHelper = new NoneGenericListHelper();
 
-//listHelper.GoTroughSrings(strings);
-//listHelper.GoTroughIntegers(ints);
+listHelper.GoTroughSrings(strings);
+listHelper.GoTroughIntegers(ints);
 
-//var genericListHelperForStrings = new GenericListHeper<string>();
-//genericListHelperForStrings.GetInfoFor(strings);
+var genericListHelperForStrings = new GenericListHeper<string>();
+genericListHelperForStrings.GetInfoFor(strings);
 
-//var genericListHelperForBooleans = new GenericListHeper<bool>();
-//genericListHelperForBooleans.GetInfoFor(bools);
-
-
-//var genericMethodHelpers = new GenericMethodHelpers();
-//genericMethodHelpers.GoTrough(strings);
-//genericMethodHelpers.GoTrough(ints);
-//genericMethodHelpers.GoTrough(bools);
+var genericListHelperForBooleans = new GenericListHeper<bool>();
+genericListHelperForBooleans.GetInfoFor(bools);
 
 
+var genericMethodHelpers = new GenericMethodHelpers();
+genericMethodHelpers.GoTrough(strings);
+genericMethodHelpers.GoTrough(ints);
+genericMethodHelpers.GoTrough(bools);
 
+Console.WriteLine("===================");
 
 var OrderDb = new GenericDb<Order>();
 var ProductDb = new GenericDb<Product>();
-
 
 OrderDb.Insert(new Order() { Id = 1, Address = "Bob street 29", Receiver = "Bob" });
 OrderDb.Insert(new Order() { Id = 2, Address = "Jill street 31", Receiver = "Jill" });
@@ -39,6 +37,5 @@ ProductDb.Insert(new Product() { Id = 3, Description = "64GB", Title = "USB" });
 
 OrderDb.PrintAll();
 ProductDb.PrintAll();
-
 
 Console.ReadLine();
