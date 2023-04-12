d73e81ed17d45119e3457740e42c1779eeef7123
Angel Mitrov <Angel.Mitrov@seavus.com>
2023-04-12 19:33:42 +0200

    Added Exercise

---
+++ G5/Class06/Exercise/SEDC.Class06/SEDC.Class06.Exercise/Classes/Dog.cs
@@ -0,0 +1,20 @@
+﻿using SEDC.Class06.Exercise.Enums;
+
+namespace SEDC.Class06.Exercise.Classes
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+        public Race Race { get; set; }
+
+        public Dog(string name, string color, int age, Race race)
+        {
+            Name = name;
+            Color = color;
+            Age = age;
+            Race = race;
+        }
+    }
+}
---
+++ G5/Class06/Exercise/SEDC.Class06/SEDC.Class06.Exercise/Enums/Enums.cs
@@ -0,0 +1,28 @@
+﻿namespace SEDC.Class06.Exercise.Enums
+{
+    public enum Job
+    {
+        Archivist,
+        Waiter,
+        Choreographer,
+        Developer,
+        Dentist,
+        Sculptor,
+        Interpreter,
+        Barber
+    }
+
+    public enum Race
+    {
+        Boxer,
+        Bulldog,
+        Collie,
+        Dalmatian,
+        Doberman,
+        Mutt,
+        Mudi,
+        Pointer,
+        Pug,
+        Plott
+    }
+}
