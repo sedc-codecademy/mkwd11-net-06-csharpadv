adc161133f9a3300f22c1ca622dc30fa7c145b5c
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-12 19:23:16 +0200

    class 6 updated

---
+++ G3/class06/code/Generics/Generics/Helpers/GenericMethodHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Helpers
+{
+    public class GenericMethodHelpers
+    {
+        public void GoTrough<T>(List<T> items)
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ G3/class06/code/Generics/Generics/Helpers/NoneGenericListHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Helpers
+{
+    public class NoneGenericListHelper
+    {
+        public void GoTroughSrings(List<string> strings) 
+        {
+            foreach (string str in strings) 
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        public void GetInfoForStrings(List<string> strings) 
+        {
+            string firstElement = strings[0];
+            Console.WriteLine($"This list has {strings.Count} " +
+                $"members and is a type of {firstElement.GetType().Name}");
+        }
+
+        public void GoTroughIntegers(List<int> ints) 
+        {
+            foreach (int num in ints) 
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        public void GetInfoForIntegers(List<int> ints)
+        {
+            int firstElement = ints[0];
+            Console.WriteLine($"This list has {ints.Count} " +
+                $"members and is a type of {firstElement.GetType().Name}");
+        }
+    }
+}
