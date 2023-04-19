d735715b64623a4b9f8f11552d67f12684f5ae69
Danilo Borozan <daniloborozan07@gmail.com>
2023-04-19 19:27:11 +0200

    Add code for Subscription and event

---
+++ G5/Class07/SubscriptionWithEvent/SubscriptionWithEvent.Class07.App/Program.cs
@@ -0,0 +1,59 @@
+﻿using SubscriptionWithEvent.Class07.Domain.Enums;
+using SubscriptionWithEvent.Class07.Domain.Models;
+
+Market KamMarket = new Market()
+{
+    Name = "KamMarket",
+    CurrentPromotion = ProductType.Food
+};
+Market VeroMarket = new Market()
+{
+    Name = "VeroMarket",
+    CurrentPromotion = ProductType.Electronics
+};
+User Bob = new User()
+{
+    Name = "Bob Bobsky",
+    Age = 21,
+    FavoriteType = ProductType.Food
+};
+User Jill = new User()
+{
+    Name = "Jill Wayne",
+    Age = 28,
+    FavoriteType = ProductType.Cosmetics
+};
+User Greg = new User()
+{
+    Name = "Greg Gregsky",
+    Age = 48,
+    FavoriteType = ProductType.Electronics
+};
+
+// Users subscribe to KamMarket for their promotions
+KamMarket.SubcriptionForPromotion(Bob.ReadPromotion, "bob@gmail.com");
+KamMarket.SubcriptionForPromotion(Jill.ReadPromotion, "jill@gmail.com");
+KamMarket.SubcriptionForPromotion(Greg.ReadPromotion, "greg@gmail.com");
+
+// Users subscribe to VeroMarket for their promotion
+VeroMarket.SubcriptionForPromotion(Bob.ReadPromotion, "bob@gmail.com");
+VeroMarket.SubcriptionForPromotion(Greg.ReadPromotion, "greg@gmail.com");
+
+Console.WriteLine("======= KAM MARKET ============");
+KamMarket.SendPromotion(); // This will send promotions to everyone who subscribed ( Will call the methods that were sent to the event )
+
+Console.WriteLine("======= VERO MARKET ============");
+VeroMarket.SendPromotion(); // VeroMarket decides to send promotions as well to everyone who subscribed
+
+KamMarket.CurrentPromotion = ProductType.Cosmetics; // The promotion is changed 
+KamMarket.UnSubcriptionForPromotion(Greg.ReadPromotion, "Boring");// Greg is not happy with KamMarket new promotions
+
+
+VeroMarket.UnSubcriptionForPromotion(Greg.ReadPromotion, "Nothing great going on in the promotions!"); // Greg is not happy with VeroMarket promotions
+
+Console.WriteLine("======= KAM MARKET ============");
+KamMarket.SendPromotion();// KamMarket sends another promotion
+
+Console.WriteLine("======= Read Complaints ============");
+KamMarket.ReadComplaints(); // KamMarket now reads the Complaints
+VeroMarket.ReadComplaints(); // VeroMarket now reads the Complaints
---
+++ G5/Class07/SubscriptionWithEvent/SubscriptionWithEvent.Class07.Domain/Enums/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionWithEvent.Class07.Domain.Enums
+{
+    public enum ProductType
+    {
+        Electronics = 1,
+        Food,
+        Cosmetics,
+        Other
+    }
+}
