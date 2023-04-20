14ed1158c9bcc41c5bb3f2efb137c7d7790da8d7
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2023-04-20 20:54:08 +0200

    Workshop part 2

---
+++ G7/Class08 (Workshop)/TryBeingFit/TryBeingFit.Models/Enums/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace TryBeingFit.Models.Enums
+{
+    public enum UserRole
+    {
+        Standard = 1,
+        Premium,
+        Trainer
+    }
+}
---
+++ G7/Class08 (Workshop)/TryBeingFit/TryBeingFit.Models/Training.cs
@@ -0,0 +1,24 @@
+﻿using TryBeingFit.Models.Enums;
+
+namespace TryBeingFit.Models
+{
+    public abstract class Training : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public double Rating { get; set; }
+        public TrainingDifficulty Difficulty { get; set; }
+        public string Link { get; set; }
+
+        public Training(string title, string description, int duration, double rating, TrainingDifficulty difficulty, string link)
+        {
+            Title = title;
+            Description = description;
+            Duration = duration;
+            Rating = rating;
+            Difficulty = difficulty;
+            Link = link;
+        }
+    }
+}
