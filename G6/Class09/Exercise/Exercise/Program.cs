a3a47607ca0f4ae718dd7372031debe65fc66fd2
tstojanovska <stojanovskatanja03@gmail.com>
2023-04-20 20:50:27 +0200

    Class 09

---
+++ G6/Class09/Exercise/Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿string Calculation(int num1, int num2)
+{
+    return $"{num1} + {num2} = {num1 + num2}";
+}
+
+string folderPath = @"..\..\..\Exercise";
+string filePath = @"..\..\..\Exercise\calculations.txt";
+
+try
+{
+    Console.WriteLine("Enter first number");
+    string firstInput = Console.ReadLine();
+
+    Console.WriteLine("Enter second number");
+    string secondInput = Console.ReadLine();
+
+    string result = Calculation(int.Parse(firstInput), int.Parse(secondInput));
+
+    if (!Directory.Exists(folderPath))
+    {
+        Directory.CreateDirectory(folderPath);
+    }
+
+    using(StreamWriter sw = new StreamWriter(filePath, true))
+    {
+        sw.WriteLine($"{DateTime.Now: dd.MM.yyyy HH:mm:ss} : {result}");
+        sw.WriteLine("========================");
+    }
+}
+catch(Exception e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
---
+++ G6/Class09/WorkingWithFileClass/WorkingWithFileClass/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+
+//absolute path
+string currentDirectory = Directory.GetCurrentDirectory();
+Console.WriteLine(currentDirectory);
+
+//relative path to console app folder
+string appPath = @"..\..\..\";
+
+// ..\..\..\myFolder -> check if this path exists, check if three levels above exists a folder called myFolder
+bool myFolderExists = Directory.Exists(appPath + "myFolder");
+Console.WriteLine($"My folder exists: {myFolderExists}");
+
+//..\..\..\newFolder
+string newFolderPath = appPath + "newFolder";
+if (!Directory.Exists(newFolderPath))
+{
+    Directory.CreateDirectory(newFolderPath);
+    Console.WriteLine("New folder succesfully created.");
+}
+
+Console.WriteLine($"New folder exists: {Directory.Exists(newFolderPath)}");
+
+if (Directory.Exists(newFolderPath))
+{
+    Directory.Delete(newFolderPath);
+    Console.WriteLine("New folder deleted");
+}
+
+//WORKING WITH FILES
+if(!Directory.Exists(appPath + "myFolder"))
+{
+    Directory.CreateDirectory(appPath + "myFolder");
+    Console.WriteLine("My folder succesfully created.");
+}
+
+//       ..\..\..\myFolder\test.txt
+bool testTxtExists = File.Exists(appPath + @"myFolder\test.txt");
+Console.WriteLine($"Test txt exists: {testTxtExists}");
+
+if (!testTxtExists)
+{
+    File.Create(appPath + @"myFolder\test.txt").Close();
+    Console.WriteLine("test.txt was created.");
+}
+
+if(File.Exists(appPath + @"myFolder\test.txt"))
+{
+    //overwrite the content of test.txt
+    File.WriteAllText(appPath + @"myFolder\test.txt", "Hello  SEDC!");
+}
+
+if (File.Exists(appPath + @"myFolder\test.txt"))
+{
+    //overwrite the content of test.txt
+    File.WriteAllText(appPath + @"myFolder\test.txt", "Hello  SEDC again! This is another text!");
+}
+
+if (File.Exists(appPath + @"myFolder\test.txt"))
+{
+    //append text to the content of test.txt
+    File.AppendAllText(appPath + @"myFolder\test.txt", Environment.NewLine + "Goodbye \n SEDC");
+}
+
+//delete
+if (File.Exists(appPath + @"myFolder\test.txt"))
+{
+    File.Delete(appPath + @"myFolder\test.txt");
+}
