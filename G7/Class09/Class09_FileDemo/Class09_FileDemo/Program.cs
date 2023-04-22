7b140464036005bb27cca5f51dfbebb91ca46abb
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2023-04-22 12:36:39 +0200

    Class 09

---
+++ G7/Class09/Class09_FileDemo/Class09_FileDemo/Program.cs
@@ -0,0 +1,99 @@
+﻿using Models;
+
+namespace Class09_FileDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Working with folders
+            #region Absolute Path
+            string absolutePath = @"C:\";
+            Directory.CreateDirectory(absolutePath + "Class09Folder"); //C:\Class09Folder
+            Directory.CreateDirectory(absolutePath + @"Class09Folder\Folder01\NestedFolder");
+            #endregion
+
+            //absolute path of the folder where our exe is.
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirectory);
+
+            //if we do not change the path, the new folder will be created in the execution folder path (the one specified above)
+            Directory.CreateDirectory("MyAppFolder");
+
+            //Class09 root folder (5 levels above the current path (exe path))
+            string class09Root = @"..\..\..\..\..\";
+
+            //relative path based on the current path
+            //Directory.CreateDirectory(currentDirectory + class09Root + "RootFolder");
+            string newFolderPath = class09Root + "RootFolder";
+            Directory.CreateDirectory(newFolderPath);
+            Directory.CreateDirectory(newFolderPath + @"\SubFolder");
+
+            //bool folderExists = Directory.Exists(class09Root + "RootFolder");
+
+            //if(folderExists)
+            //{
+            //    Directory.Delete(class09Root + "RootFolder");
+            //}
+
+            //Renaming
+            string newPath = class09Root + @"RenamedFolder";
+            Directory.Move(newFolderPath, newPath);
+
+            if (Directory.Exists(newPath))
+            {
+                //removes the folder only if it is empty
+                //Directory.Delete(newFolderPath);
+
+                //remove the folder and all its content
+                Directory.Delete(newPath, true);
+            }
+            #endregion
+
+            string folderFilePath = "WorkingWithFiles";
+            Directory.CreateDirectory(folderFilePath);
+            string filePath = folderFilePath + @"\example.txt";
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            //string fileJsonPath = folderFilePath + @"\example.json";
+            //if (!File.Exists(fileJsonPath))
+            //{
+            //    File.Create(fileJsonPath).Close();
+            //}
+
+            if(File.Exists(filePath))
+            {
+                //write some content in the file
+                File.WriteAllText(filePath, "Hello everyone, welcome to SEDC\nThis is our class");
+            }
+
+            //WriteAllText replace (overwrites) the previous content
+            if (File.Exists(filePath))
+            {
+                //write some content in the file
+                File.WriteAllText(filePath, "Hello this is a new text");
+            }
+
+            if (File.Exists(filePath))
+            {
+                //appends some content in the file
+                File.AppendAllText(filePath, "\nThis is additional text");
+            }
+
+            if (File.Exists(filePath))
+            {
+                //read file content
+                string text = File.ReadAllText(filePath);                
+            }
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
---
+++ G7/Class09/Class09_StreamsDemo/Class09_LoggerDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Class09_LoggerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> names = new List<string> { "risto" };
+
+                Console.WriteLine(names[5]);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("An error happend, please try again later");
+
+                string folderPath = @"..\..\..\Logs";
+                string filePath = folderPath + @$"\logs_{DateTime.UtcNow.ToString("dd-MM-yyyy")}.txt";
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath).Close();
+                }
+
+                //Opens a streamWriter that is a disposable object that will be closed (damaged) once the using block finishes
+                //Creating a StreamWriter only with filePath, creates a streamwriter with the option to override the content of that file.
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.Write($"{DateTime.UtcNow} Error: {ex}");
+                }
+            }
+        }
+    }
+}
