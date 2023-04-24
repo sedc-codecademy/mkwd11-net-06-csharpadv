c14dde26b239cf345617d2aeedd56ded12cc9417
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-24 19:34:33 +0200

    class 07 updates

--- G3/class07/code/ActionAndFunc/ActionAndFunc/Program.cs
+++ G3/class07/code/ActionAndFunc/ActionAndFunc/Program.cs
@@ -6,20 +6,68 @@
 //ACTION
 //action is always void
 
-Action hello = () => Console.WriteLine("Hello there dragan!");
+Action hello = () => Console.WriteLine("Hello there!");
 
-Action<string> printRed = color =>
+Action<string> printRed = message =>
 {
     Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(color);
+    Console.WriteLine(message);
     Console.ResetColor();
 };
 
-Action<string, int, int, bool> 
-static void DoSomething(Action<string> doSomethingAction) 
+Action<string, ConsoleColor> printColor = (message, color) =>
 {
-    Console.WriteLine("inside Do Something");
-    doSomethingAction("inside doSomethingAction");
-}
+    Console.ForegroundColor = color;
+    Console.WriteLine(message);
+    Console.ResetColor();
+};
 
-DoSomething(printRed);
+hello();
+printRed("some error message");
+printColor("some error message", ConsoleColor.Magenta);
+
+
+//static void DoSomething(string message, Action<string> doSomethingAction)
+//{
+//    Console.WriteLine("inside Do Something");
+//    doSomethingAction(message);
+//    Console.WriteLine("inside Do Something 2");
+//}
+
+//DoSomething("some message from outside", message => 
+//{
+//    Console.ForegroundColor = ConsoleColor.Yellow;
+//    Console.WriteLine(message);
+//    Console.ResetColor();
+//});
+
+//DoSomething("some message from outside 2", message =>
+//{
+//    Console.ForegroundColor = ConsoleColor.Magenta;
+//    Console.WriteLine(message);
+//    Console.ResetColor();
+//});
+
+//FUNCTION
+//function always has a return value
+
+Func<bool> isNameEmpty = () => names.Count == 0;
+
+Func<List<string>, bool> isListEmpty = arr => arr.Count == 0;
+
+Func<int, int, int> sum = (number1, number2) => number1 + number2;
+
+Func<int, int, bool> checkIfLarger = (number1, number2) =>
+{
+    if (number1 > number2) 
+    {
+        return true;
+    }
+
+    return false;
+};
+
+Console.WriteLine(isNameEmpty());
+Console.WriteLine(isListEmpty(names));
+Console.WriteLine(sum(5, 10));
+Console.WriteLine(checkIfLarger(4, 8));
