f9676a205ebe83c3977a13103d3616c9d93d2871
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-24 18:40:55 +0200

    class 07 updated

--- G3/class07/code/ActionAndFunc/ActionAndFunc/Program.cs
+++ G3/class07/code/ActionAndFunc/ActionAndFunc/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿List<string> names = new List<string>()
+{
+    "Bob", "Jill", "Wayne", "Greg", "John", "Anne"
+};
+
+//ACTION
+//action is always void
+
+Action hello = () => Console.WriteLine("Hello there dragan!");
+
+Action<string> printRed = color =>
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(color);
+    Console.ResetColor();
+};
+
+Action<string, int, int, bool> 
+static void DoSomething(Action<string> doSomethingAction) 
+{
+    Console.WriteLine("inside Do Something");
+    doSomethingAction("inside doSomethingAction");
+}
+
+DoSomething(printRed);
