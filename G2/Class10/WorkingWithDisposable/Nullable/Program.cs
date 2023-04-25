0f802093a3756d3fa345ceff37757226727458c3
Bojan Damchevski <bod@proofficepark.dk>
2023-04-25 20:38:12 +0200

    Code class 10

---
+++ G2/Class10/WorkingWithDisposable/Nullable/Program.cs
@@ -0,0 +1,12 @@
+﻿using Nullable;
+
+Person person = new Person();
+Console.WriteLine(person.Id); //is 0 by default
+Console.WriteLine(person.Score); // is null by default
+
+Console.WriteLine(person.Wife); // because Wife is a class, it is nullable
+
+person.HasPet = null;
+//person.Id = null; this can not be !
+
+person = null;
