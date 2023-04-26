1d6aa9cc81bfefa09c84382bda76038e9627ee9c
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-26 19:41:05 +0200

    helpers added

---
+++ G3/class07/code/AdvancedLinq/AdvancedLinq/Helpers/ListHelper.cs
@@ -0,0 +1,34 @@
+﻿using AdvancedLinq.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedLinq.Helpers
+{
+    public static class ListHelper
+    {
+        public static void PrintSimple<T>(this List<T> list) 
+        {
+            Console.WriteLine("Printing...");
+            Console.WriteLine("------------------------------");
+            foreach (T item in list) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------------------------");
+        }
+
+        public static void PrintEntites<T>(this List<T> list) where T : BaseEntity 
+        {
+            Console.WriteLine($"Printing {list[0].GetType().Name}s...");
+            Console.WriteLine("------------------------------");
+            foreach (T item in list)
+            {
+                Console.WriteLine(item.Info());
+            }
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
