7c95699e9853c175082bc5d7887589b29938a9ad
Danilo Borozan <daniloborozan07@gmail.com>
2023-04-26 20:32:24 +0200

    Add code for Class 10

---
+++ G5/Class10/SEDC.Class10.Nullable/SEDC.Class10.Nullable/Program.cs
@@ -0,0 +1,29 @@
+﻿using SEDC.Class10.Nullable.Domain;
+
+Person person = new Person();
+
+//person = null; // null
+
+Console.WriteLine(person.Id); //0 by default
+Console.WriteLine(person.Score); //null by default
+
+Console.WriteLine(person.Name); //null by default
+person.Name = null;
+
+Console.WriteLine(person.IsStudent); // false by default
+Console.WriteLine(person.IsEmployed); //null by default
+
+//person.IsStudent = null;
+
+person.IsEmployed = null;
+
+
+//Nullabale method
+person.isTrue(null);
+person.isFalse();
+person.Number();
+person.Text();
+
+Person personTwo = new Person();
+
+personTwo = null; //null
