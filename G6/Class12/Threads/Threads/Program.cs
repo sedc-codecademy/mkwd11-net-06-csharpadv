b27d69ef3dd3bcd44a7218549c4ed6a245fa5d69
tstojanovska <stojanovskatanja03@gmail.com>
2023-04-27 20:47:00 +0200

    Class 12

---
+++ G6/Class12/Threads/Threads/Program.cs
@@ -0,0 +1,36 @@
+﻿//synchronous
+void PrintMessagesSynchronous()
+{
+    Console.WriteLine("Hello");
+    Console.WriteLine("First message"); //call to api/ call to service
+    Thread.Sleep(3000); //wait for response
+    Console.WriteLine("Second message"); //validate the response
+    Console.WriteLine("Third message");
+    Console.WriteLine("Bye");
+}
+
+//PrintMessagesSynchronous();
+
+
+void PrintMessagesInDifferentThreads()
+{
+    Console.WriteLine("Hello");
+
+    Thread firstThread = new Thread(() =>
+    {
+        //Thread.Sleep(1000);
+        Console.WriteLine("First message");
+    });
+    firstThread.Start();
+
+    Thread secondThread = new Thread(() =>
+    {
+        //Thread.Sleep(1000);
+        Console.WriteLine("Second message");
+    });
+    secondThread.Start();
+
+    Console.WriteLine("Bye");
+}
+
+PrintMessagesInDifferentThreads();
