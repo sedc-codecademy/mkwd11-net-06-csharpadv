97220c7aec590634708b78e2a61f74e8fbca1e89
Viktor Jakovlev <vicebad@hotmail.com>
2023-04-29 12:48:29 +0200

    events example added

---
+++ G3/class08/code/Events/Events/Entities/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Entities
+{
+    public enum ProductType
+    {
+        Food,
+        Cosmetics,
+        Electronics,
+        Other
+    }
+}
---
+++ G3/class08/code/Events/Events/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Entities
+{
+    // This class is a subscriber class
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public ProductType FavoriteType { get; set; }
+
+        // A method that can read a promotion and get excited if that is the favorite type of this person
+        public void ReadPromotion(ProductType product) 
+        {
+            Console.WriteLine($"Mr/Ms: {Name}, The product {product} is on sale");
+            if (product == FavoriteType) 
+            {
+                Console.WriteLine("MY FAVORITE!");
+            }
+        }
+    }
+}
