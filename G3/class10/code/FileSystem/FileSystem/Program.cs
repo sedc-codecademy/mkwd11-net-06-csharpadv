25d0410cf590d6ac2587aca7c9ed32306e32ce3b
Viktor Jakovlev <vicebad@hotmail.com>
2023-05-03 18:52:24 +0200

    class 10 init

---
+++ G3/class10/code/FileSystem/FileSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string currentDirectory = Directory.GetCurrentDirectory();
+string appPath = @"..\..\..\";
+
+//relative path 
+bool myFolderExist = Directory.Exists(appPath + "myFolder");
+
+//absolute path
+bool myFolderExistAbsolute = Directory.Exists(@"C:\Users\viceb\Desktop\mkwd11-net-06-csharpadv\G3\class10\code\FileSystem\FileSystem\myFolder");
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
