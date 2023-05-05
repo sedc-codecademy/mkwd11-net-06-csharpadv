b4f1fdfe56a1efd64c2718bff0f8a4bd82bca3ef
Viktor Jakovlev <vicebad@hotmail.com>
2023-05-05 19:18:59 +0200

    class 11 updated

--- G3/class11/code/Disposable/Disposable/Program.cs
+++ G3/class11/code/Disposable/Disposable/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Disposable.CustomReaderWriter;
+
 string appPath = @"..\..\..\Text";
 string filePath = Path.Combine(appPath, "text.txt");
 
@@ -17,6 +18,7 @@ void CreateFile(string path)
     }
 }
 
+// manual disposing
 void ManualExample() 
 {
     try
@@ -25,17 +27,17 @@ void ManualExample()
         string text1 = Console.ReadLine();
         AppendTextInFile(text1, filePath);
 
-        //Console.WriteLine("Enter text pt2: ");
-        //string text2 = Console.ReadLine();
-        //AppendTextInFile(text2, filePath);
+        Console.WriteLine("Enter text pt2: ");
+        string text2 = Console.ReadLine();
+        AppendTextInFile(text2, filePath);
 
-        //Console.WriteLine("Enter text pt3: ");
-        //string text3 = Console.ReadLine();
-        //AppendTextInFile(text3, filePath);
+        Console.WriteLine("Enter text pt3: ");
+        string text3 = Console.ReadLine();
+        AppendTextInFile(text3, filePath);
     }
     catch (Exception ex)
     {
-        Console.WriteLine("There wasa problem in the writing system.");
+        Console.WriteLine("There was problem in the writing system.");
         Console.WriteLine(ex.Message);
     }
 
@@ -52,7 +54,6 @@ void ManualExample()
         Console.WriteLine(ex.Message);
     }
 }
-
 void AppendTextInFile(string text, string path) 
 {
     StreamWriter sw = new StreamWriter(path, true);
@@ -65,7 +66,6 @@ void AppendTextInFile(string text, string path)
     sw.WriteLine(text);
     sw.Dispose();
 }
-
 void ReadTextFromFile(string path) 
 {
     StreamReader sr = new StreamReader(path);
@@ -76,13 +76,110 @@ void ReadTextFromFile(string path)
     sr.Dispose();
 }
 
+// disposing with using keyword
+void UsingExample() 
+{
+    try
+    {
+        Console.WriteLine("Enter text pt1: ");
+        string text1 = Console.ReadLine();
+        AppendTextInFileSafe(text1, filePath);
 
+        Console.WriteLine("Enter text pt2: ");
+        string text2 = Console.ReadLine();
+        AppendTextInFileSafe(text2, filePath);
 
+        Console.WriteLine("Enter text pt3: ");
+        string text3 = Console.ReadLine();
+        AppendTextInFileSafe(text3, filePath);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("There wasa problem in the writing system.");
+        Console.WriteLine(ex.Message);
+    }
 
+    try
+    {
+        Console.ReadLine();
+        Console.WriteLine("---------Read(---------");
+
+        ReadTextFromFile(filePath);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("There was problem in the writing system.");
+        Console.WriteLine(ex.Message);
+    }
+}
+void AppendTextInFileSafe(string text, string path) 
+{
+    using (StreamWriter sw = new StreamWriter(path, true))
+    {
+        if (text == "break") 
+        {
+            throw new Exception("Something broke unexpectedly...");
+        }
+
+        sw.WriteLine(text);
+    }
+}
+
+// custom displosable example
+void OurDisposableExample() 
+{
+    try
+    {
+        Console.WriteLine("Enter text pt1: ");
+        string text1 = Console.ReadLine();
+        AppendTextInFileOurExample(text1, filePath);
+
+        Console.WriteLine("Enter text pt2: ");
+        string text2 = Console.ReadLine();
+        AppendTextInFileOurExample(text2, filePath);
+
+        Console.WriteLine("Enter text pt3: ");
+        string text3 = Console.ReadLine();
+        AppendTextInFileOurExample(text3, filePath);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("There wasa problem in the writing system.");
+        Console.WriteLine(ex.Message);
+    }
+
+    try
+    {
+        Console.ReadLine();
+        Console.WriteLine("---------Read---------");
+
+        ReadTextFromFileOurExample(filePath);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("There wasa problem in the writing system.");
+        Console.WriteLine(ex.Message);
+    }
+}
+void AppendTextInFileOurExample(string text, string path) 
+{
+    using (CustomWriter cw = new CustomWriter(path)) 
+    {
+        cw.Write(text);
+    }
+}
+void ReadTextFromFileOurExample(string path) 
+{
+    using (CustomReader cr = new CustomReader(path)) 
+    {
+        Console.WriteLine(cr.Read());
+    }
+}
 
 CreateFolder(appPath);
 CreateFile(filePath);
 
-ManualExample();
-
+//ManualExample();
+//UsingExample();
+OurDisposableExample();
 
---
+++ G3/class11/code/Nullable/Nullable/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int? Score { get; set; }
+        public bool IsEmployed { get; set; }
+        public bool? HasPet { get; set; }
+
+        public string Name { get; set; }
+        public string? Username { get; set; }
+    }
+}
