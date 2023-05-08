45a06561fbc1b58ed719d3151697205ff17aea3a
Viktor Jakovlev <vicebad@hotmail.com>
2023-05-08 18:36:33 +0200

    class 12 updated

---
+++ G3/class12/code/SerializeDeserialize/SerializeDeserialize/Services/CustomJsonService.cs
@@ -0,0 +1,29 @@
+﻿using SerializeDeserialize.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeserialize.Services
+{
+    public class CustomJsonService
+    {
+        public string SerializeStudent(Student student) 
+        {
+            string json = "{";
+            json += $"\"FirstName\": \"{student.FirstName}\",";
+            json += $"\"LastName\": \"{student.LastName}\",";
+            json += $"\"Age\": \"{student.Age}\",";
+            json += $"\"IsPartTime\": \"{student.IsPartTime.ToString().ToLower()}\"";
+            json += "}";
+
+            return json;
+        }
+
+        public Student DeserializeStudent(string json) 
+        {
+            return new Student();
+        }
+    }
+}
