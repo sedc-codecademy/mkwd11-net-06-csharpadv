c6e48a8a7bf1b06a412d00f7500e0b96b057d69f
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2023-05-09 13:40:26 +0200

    Workshop

---
+++ G7/Class14 (Workshop)/TryBeingFit/TryBeingFit.Models/Database/Database.cs
@@ -0,0 +1,83 @@
+﻿namespace TryBeingFit.Models.Database
+{
+    // ltDatabase = new Database<LiveTraining>();
+    // LiveTraining l1 = new LiveTraingin....;
+    // ltDatabase.Insert(l1);
+    public class Database<T> : IDatabase<T> where T : BaseEntity
+    {
+        private List<T> _dataset { get; set; }
+
+        public Database()
+        {
+            _dataset = new List<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dataset;
+        }
+
+        public T GetById(int id)
+        {
+            T item = _dataset.FirstOrDefault(x => x.Id == id);
+
+            if(item == null)
+            {
+                throw new Exception($"Entity with id {id} is not found");
+            }
+
+            return item;
+        }
+
+        public int Insert(T entity)
+        {
+            _dataset.Add(entity);
+            return entity.Id;
+        }
+
+        public void Remove(T entity)
+        {
+            T item = _dataset.FirstOrDefault(x => x.Id == entity.Id);
+
+            if (item == null)
+            {
+                throw new Exception($"Entity with id {entity.Id} is not found");
+            }
+
+            _dataset.Remove(item);
+        }
+
+        public void RemoveById(int id)
+        {
+            T item = _dataset.FirstOrDefault(x => x.Id == id);
+
+            if (item == null)
+            {
+                throw new Exception($"Entity with id {id} is not found");
+            }
+
+            _dataset.Remove(item);
+        }
+
+        //Program.cs;
+        //lv = ltDatabase.GetById(2); => {2, "Jump and Jacks", 30 }
+        //lv.Duration = 60;
+        //ltDatabase.Update(lv) => {2, "Jump and Jacks", 60 }
+        public void Update(T entity)
+        {
+            //firstly find the item in the list
+            //original value => {2, "Jump and Jacks", 30 }
+            T item = _dataset.FirstOrDefault(x => x.Id == entity.Id);
+
+            if (item == null)
+            {
+                throw new Exception($"Entity with id {entity.Id} is not found");
+            }
+
+            //if the item exists, then by changing its value in the varaible, we are changing the list item;
+            //originalValue = {2, "Jump and Jacks", 60 }
+            int indexOfElement = _dataset.IndexOf(item);
+            _dataset[indexOfElement] = entity;
+        }
+    }
+}
---
+++ G7/Class14 (Workshop)/TryBeingFit/TryBeingFit.Services/Seeder/DatabaseSeeder.cs
@@ -0,0 +1,41 @@
+﻿using TryBeingFit.Models;
+using TryBeingFit.Models.Database;
+using TryBeingFit.Models.Enums;
+
+namespace TryBeingFit.Services.Seeder
+{
+    public class DatabaseSeeder
+    {
+        private IDatabase<User> _userDatabase;
+        private IDatabase<Training> _trainingDatabase;
+
+        public DatabaseSeeder(IDatabase<User> userDatabase, IDatabase<Training> trainingDatabase)
+        {
+            _userDatabase = userDatabase;
+            _trainingDatabase = trainingDatabase;
+        }
+
+        public void Seed()
+        {
+            Trainer trainer1 = new Trainer("Elena", "Bubalo", "elenaB", "elenaB123", "elena@gmail.com", "070123456", 10, "");
+            Trainer trainer2 = new Trainer("Ana", "Stojanovska", "anaS", "anaS123", "ana@gmail.com", "070654321", 5, "");
+
+            LiveTraining l1 = new LiveTraining("HIT training", "No equip needed", 30, 4.5, TrainingDifficulty.Advanced, "link 1", DateTime.Now.AddDays(2), trainer1);
+            LiveTraining l2 = new LiveTraining("Yoga", "Mat", 60, 4, TrainingDifficulty.Beginner, "link 2", DateTime.Now.AddDays(7), trainer2);
+
+            trainer1.LiveTrainings.Add(l1);
+            trainer2.LiveTrainings.Add(l2);
+
+            VideoTraining v1 = new VideoTraining("Cardio training", "None", 50, 4, TrainingDifficulty.Beginner, "link 2");
+            VideoTraining v2 = new VideoTraining("Zumba training", "None", 20, 3.5, TrainingDifficulty.Advanced, "link 5");
+
+            _userDatabase.Insert(trainer1);
+            _userDatabase.Insert(trainer2);
+
+            _trainingDatabase.Insert(l1);
+            _trainingDatabase.Insert(l2);
+            _trainingDatabase.Insert(v1);
+            _trainingDatabase.Insert(v2);
+        }
+    }
+}
