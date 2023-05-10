80c420436eec46ee1361924f8b625b96033e57f3
Viktor Jakovlev <vicebad@hotmail.com>
2023-05-10 19:06:02 +0200

    exercise updated

--- G3/class13/code/FileSystemDatabase/FileSystemDatabase/Db.cs
+++ G3/class13/code/FileSystemDatabase/FileSystemDatabase/Db.cs
@@ -14,6 +14,8 @@ namespace FileSystemDatabase
         private string _dbDirectory;
         private string _dbFile;
 
+        private int _idTracker;
+
         public Db()
         {
             _dbDirectory = @"..\..\..\Database";
@@ -30,28 +32,66 @@ namespace FileSystemDatabase
             }
 
             List<T> data = Read();
+
+            if (data == null)
+            {
+                // write with empty list            
+            }
+            else if (data.Count > 0) 
+            {
+                _idTracker = data[data.Count - 1].Id;
+            }
         }
 
         private List<T> Read()
         {
-            using (StreamReader sr = new StreamReader(_dbFile)) 
+            try
             {
-                string data = sr.ReadToEnd();
-                return JsonConvert.DeserializeObject<List<T>>(data);
+                using (StreamReader sr = new StreamReader(_dbFile))
+                {
+                    string data = sr.ReadToEnd();
+                    return JsonConvert.DeserializeObject<List<T>>(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<T>();
             }
         }
 
+        private bool Write(List<T> entites) 
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(_dbFile))
+                {
+                    string data = JsonConvert.SerializeObject(entites);
+                    sw.Write(data);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
 
         public List<T> GetAll() 
-        {      
+        {
+            return Read();
         }
 
         public T GetById(int id) 
         {
+            List<T> data = Read();
+            return data.SingleOrDefault(entity => entity.Id == id);
         }
 
         public int Insert(T entity) 
         {
+
         }
 
         public void ClearDb() 
