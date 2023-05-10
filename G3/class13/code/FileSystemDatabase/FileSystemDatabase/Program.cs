f9dcd1773dc7d02c116550a8496a162d6124f1c8
Viktor Jakovlev <vicebad@hotmail.com>
2023-05-10 20:01:03 +0200

    exercise finished

--- G3/class13/code/FileSystemDatabase/FileSystemDatabase/Program.cs
+++ G3/class13/code/FileSystemDatabase/FileSystemDatabase/Program.cs
@@ -23,3 +23,60 @@ void Seed()
     }
 }
 Seed();
+
+//check if GetAll() works
+List<Student> Students = studentsDb.GetAll();
+List<Subject> Subjects = subjectsDb.GetAll();
+
+foreach (var student in Students)
+{
+    Console.WriteLine(student.Info());
+}
+
+foreach (var subject in Subjects)
+{
+    Console.WriteLine(subject.Info());
+}
+
+//check if GetById() works
+
+var st = studentsDb.GetById(2);
+var su = subjectsDb.GetById(1);
+
+Console.WriteLine(st);
+Console.WriteLine(su);
+
+// check if Insert() works
+
+var newStudent = new Student("Dragan", "Manaskov", 21);
+//var newStudentId = studentsDb.Insert(newStudent);
+
+//Console.WriteLine($"Student with id: {newStudentId} was insert in the database!");
+
+//foreach (var student in studentsDb.GetAll())
+//{
+//    Console.WriteLine(student.Info());
+//}
+
+// check if Delete() works
+
+studentsDb.Delete(1);
+
+var test = studentsDb.GetAll();
+
+Console.WriteLine("=======");
+
+foreach (var student in studentsDb.GetAll())
+{
+    Console.WriteLine(student.Info());
+}
+
+
+
+
+studentsDb.ClearDb();
+
+
+Console.ReadLine();
+
+
