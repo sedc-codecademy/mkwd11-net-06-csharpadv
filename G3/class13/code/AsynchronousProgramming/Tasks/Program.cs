027a7a7d537ff9bab5621716477bf3fede541b0e
Viktor Jakovlev <vicebad@hotmail.com>
2023-05-12 18:55:31 +0200

    class 13 updated

---
+++ G3/class13/code/AsynchronousProgramming/Tasks/Program.cs
@@ -0,0 +1,11 @@
+﻿Task<int> valueTask = new Task<int>(() =>
+{
+    Thread.Sleep(3000);
+    Console.WriteLine("We can now get the number...");
+    return 6;
+});
+
+valueTask.Start();
+
+
+
---
+++ G3/class13/code/AsynchronousProgramming/Threads/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+
+void SendMessages() 
+{
+    Console.WriteLine("Getting ready...");
+    Thread.Sleep(2000);
+    Console.WriteLine("First message arrived");
+    Thread.Sleep(2000);
+    Console.WriteLine("Second message arrived");
+    Thread.Sleep(2000);
+    Console.WriteLine("Third message arrived");
+    Console.WriteLine("All messages are recieved!");
+    Console.ReadLine();
+}
+
+void SendMessagesWithThreads() 
+{
+    Console.WriteLine("Getting ready...");
+
+    Thread myThread = new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("First message arrived");
+    });
+
+    myThread.Start();
+
+    new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Second message arrived");
+    }).Start();
+
+    new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Third message arrived");
+    }).Start();
+
+    Console.WriteLine("All messages are recieved!");
+}
+
+void SendMessagesWithThreads2()
+{
+    Console.WriteLine("Getting ready...");
+
+    Thread myThread = new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("4 message arrived");
+    });
+
+    myThread.Start();
+
+    new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("5 message arrived");
+    }).Start();
+
+    new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("6 message arrived");
+    }).Start();
+
+    Console.WriteLine("All messages are recieved!");
+}
+
+//SendMessages();
+SendMessagesWithThreads();
+SendMessagesWithThreads2();
